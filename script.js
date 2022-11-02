0c434e51bc9123dd8f4188bec9c7df2dbbbe16c4
Kristina <crsbka@bk.ru>
2022-11-02 19:25:12 -0400

    Add Navbar, onClick for video instead of btn

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
+// Get the video
+var video = document.getElementById("myVideo");
 
-    // Get the video
-    var video = document.getElementById("myVideo");
 
-    // Get the button
-    var btn = document.getElementById("myBtn");
-
-    // Pause and play the video, and change the button text
-    function myFunction() {
+// Pause and play the video, and change the button text
+function myFunction() {
     if (video.paused) {
-    video.play();
-    btn.innerHTML = "Pause";
-} else {
-    video.pause();
-    btn.innerHTML = "Play";
-}
+        video.play();
+    } else {
+        video.pause();
+    }
 }
